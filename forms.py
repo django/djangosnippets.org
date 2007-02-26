582a2edd22a66648a7e61be4ca66dd7202f773e9
ubernostrum <ubernostrum@5ef273d4-da28-0410-a235-ed8c9e61cd8d>
2007-02-26 05:15:45 +0000

    Trying out ModelChoiceField
    git-svn-id: http://cab.googlecode.com/svn/trunk@56 5ef273d4-da28-0410-a235-ed8c9e61cd8d

--- forms.py
+++ forms.py
@@ -16,15 +16,11 @@ class AddSnippetForm(forms.Form):
     Form used for adding Snippets.
     
     """
-    def __init__(self, *args, **kwargs):
-        super(AddSnippetForm, self).__init__(*args, **kwargs)
-        self.fields['language'].choices = [('', '----------')] + [(lang.id, lang.name) for lang in Language.objects.all()]
-    
     title = forms.CharField(max_length=250, widget=forms.TextInput(attrs=attrs_dict))
     description = forms.CharField(widget=forms.Textarea(attrs=attrs_dict))
     code = forms.CharField(widget=forms.Textarea(attrs=attrs_dict))
     tag_list = forms.CharField(max_length=250, widget=forms.TextInput(attrs=attrs_dict))
-    language = forms.ChoiceField(choices=(), widget=forms.Select(attrs=attrs_dict))
+    language = forms.ModelChoiceField(queryset=Language.objects.all(), widegt=forms.Select(attrs=attrs_dict))
 
 
 class EditSnippetForm(forms.Form):
