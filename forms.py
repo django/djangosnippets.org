39c0df407a9d79382e1516dae827fd652f77f580
ubernostrum <ubernostrum@5ef273d4-da28-0410-a235-ed8c9e61cd8d>
2007-02-26 05:58:33 +0000

    And switch it back
    git-svn-id: http://cab.googlecode.com/svn/trunk@67 5ef273d4-da28-0410-a235-ed8c9e61cd8d

--- forms.py
+++ forms.py
@@ -16,11 +16,15 @@ class AddSnippetForm(forms.Form):
     Form used for adding Snippets.
     
     """
+    def __init__(self, *args, **kwargs):
+        super(AddSnippetForm, self).__init__(*args, **kwargs)
+        self.fields['language'].choices = [(lang.id, lang.name) for lang in Language.objects.all()]
+    
     title = forms.CharField(max_length=250, widget=forms.TextInput(attrs=attrs_dict))
     description = forms.CharField(widget=forms.Textarea(attrs=attrs_dict))
     code = forms.CharField(widget=forms.Textarea(attrs=attrs_dict))
     tag_list = forms.CharField(max_length=250, widget=forms.TextInput(attrs=attrs_dict))
-    language = forms.ModelChoiceField(queryset=Language.objects.all(), widget=forms.Select(attrs=attrs_dict))
+    language = forms.ChoiceField(choices=(), widget=forms.Select(attrs=attrs_dict))
 
 
 class EditSnippetForm(forms.Form):
