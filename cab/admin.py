b2cfffdc421b7138677e127ff995fc44fde59d06
Charles Leifer <coleifer@gmail.com>
2010-06-14 08:24:49 -0500

    Making the snippets admin more useful

--- cab/admin.py
+++ cab/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+
 from cab.models import Language, Snippet
+
 
 class LanguageAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
 
+
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'rating_score', 'pub_date')
+    list_filter = ('language',)
+    date_hierarchy = 'pub_date'
+    search_fields = ('author__username', 'title', 'description', 'code',)
+
+
 admin.site.register(Language, LanguageAdmin)
-admin.site.register(Snippet)
+admin.site.register(Snippet, SnippetAdmin)
