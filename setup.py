597af18579be60c3c67643dafc7e7ddcf15940ab
Charles Leifer <coleifer@gmail.com>
2010-07-04 11:28:57 -0500

    Fixing a copy/paste foul in the setup.py ... oops

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-from relationships import VERSION
+from cab import VERSION
 
 f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 readme = f.read()
