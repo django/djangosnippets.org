2d8ad3579cf1ed568c08dd9a65a519172b088cdd
Jannis Leidel <jannis@leidel.info>
2012-10-15 16:15:41 +0200

    Use new path of auth context processor.

--- djangosnippets/settings/base.py
+++ djangosnippets/settings/base.py
@@ -66,7 +66,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
