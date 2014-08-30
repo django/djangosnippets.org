9a364a2967246e33570f8a64fe5390deed2ddb21
Jannis Leidel <jannis@leidel.info>
2014-08-30 12:42:49 +0200

    Use named URL patterns for the feeds.

--- cab/urls/feeds.py
+++ cab/urls/feeds.py
@@ -4,11 +4,11 @@ from cab import feeds
 
 urlpatterns = patterns('',
     url(r'^author/(?P<username>[\w.@+-]+)/$',
-        feeds.SnippetsByAuthorFeed()),
+        feeds.SnippetsByAuthorFeed(), name='cab_feed_author'),
     url(r'^language/(?P<slug>[\w-]+)/$',
-        feeds.SnippetsByLanguageFeed()),
+        feeds.SnippetsByLanguageFeed(), name='cab_feed_language'),
     url(r'^latest/$',
-        feeds.LatestSnippetsFeed()),
+        feeds.LatestSnippetsFeed(), name='cab_feed_latest'),
     url(r'^tag/(?P<slug>[\w-]+)/$',
-        feeds.SnippetsByTagFeed()),
+        feeds.SnippetsByTagFeed(), name='cab_feed_tag'),
 )
