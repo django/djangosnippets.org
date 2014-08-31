87969b6c020bf7050ba6ecfa15f047e28d5b7646
Jannis Leidel <jannis@leidel.info>
2014-08-31 14:46:42 +0200

    Fixed users URL path.

--- cab/urls/users.py
+++ cab/urls/users.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 from cab.views import popular, snippets
 
 urlpatterns = patterns('',
-    url(r'^users/$',
+    url(r'^$',
         popular.top_authors,
         name='cab_top_authors'),
-    url(r'^users/(?P<username>[-\w]+)/$',
+    url(r'^(?P<username>[-\w]+)/$',
         snippets.author_snippets,
         name='cab_author_snippets'),
 )
