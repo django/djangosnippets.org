d2461563a109e81bc9059e60125b7bd060f0b249
Jannis Leidel <jannis@leidel.info>
2014-08-31 21:33:38 +0200

    Allow dots in usernames.

--- djangosnippets/urls.py
+++ djangosnippets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, patterns, include
 from django.contrib import admin
 from django.shortcuts import render
+from cab.views import snippets
 
 admin.autodiscover()
 
@@ -16,6 +17,9 @@ urlpatterns = patterns('',
     url(r'^search/', include('cab.urls.search')),
     url(r'^snippets/', include('cab.urls.snippets')),
     url(r'^tags/', include('cab.urls.tags')),
+    url(r'^users/(?P<username>[\.-\w]+)/$',
+        snippets.author_snippets,
+        name='cab_author_snippets'),
     url(r'^users/', include('cab.urls.users')),
     url(r'^$', lambda request: render(request, 'homepage.html'), name='home'),
 )
