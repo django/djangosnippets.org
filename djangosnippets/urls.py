7210f8715b39c8aacfea7e4d777a26bce0efd351
Jannis Leidel <jannis@leidel.info>
2014-08-31 21:26:50 +0200

    Prevent deprecation warning.

--- djangosnippets/urls.py
+++ djangosnippets/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import url, patterns, include
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
 from django.shortcuts import render
 
