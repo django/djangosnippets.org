8effee9420da7d255e82092f6e7109fb7ca96efa
Jannis Leidel <jannis@leidel.info>
2014-09-03 22:35:20 +0200

    Removed disqus app again.

--- djangosnippets/settings/base.py
+++ djangosnippets/settings/base.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = (
     'south',
     'taggit',
     'captcha',
-    'disqus',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -124,5 +123,3 @@ CAPTCHA_NOISE_FUNCTIONS = (
 )
 CAPTCHA_BACKGROUND_COLOR = '#ffffff'
 CAPTCHA_FOREGROUND_COLOR = '#316241'
-
-DISQUS_WEBSITE_SHORTNAME = 'djangosnippets'
