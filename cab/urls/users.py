38c1fa56c928f0b66d37d676cba8995acff129eb
Jannis Leidel <jannis@leidel.info>
2014-09-15 10:35:37 +0200

    Use better username pattern.

--- cab/urls/users.py
+++ cab/urls/users.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     url(r'^$',
         popular.top_authors,
         name='cab_top_authors'),
-    url(r'^(?P<username>[-\w]+)/$',
+    url(r'^(?P<username>[\w.@+-]+)/$',
         snippets.author_snippets,
         name='cab_author_snippets'),
 )
