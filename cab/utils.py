26fcea64463c86ac5e2bf4785e4932e361131f5b
Tim Graham <timograham@gmail.com>
2016-09-02 13:53:20 -0400

    Fix deprecated template rendering

--- cab/utils.py
+++ cab/utils.py
@@ -3,13 +3,12 @@ import datetime
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import InvalidPage, Paginator
 from django.http import Http404, HttpResponse
-from django.template import RequestContext, loader
+from django.template import loader
 
 
 def object_list(request, queryset, paginate_by=None, page=None,
                 allow_empty=True, template_name=None, template_loader=loader,
-                extra_context=None, context_processors=None,
-                template_object_name='object', content_type=None):
+                extra_context=None, template_object_name='object', content_type=None):
     """
     Generic list of objects.
 
@@ -73,7 +72,7 @@ def object_list(request, queryset, paginate_by=None, page=None,
         except InvalidPage:
             previous_page = None
 
-        c = RequestContext(request, {
+        c = {
             '%s_list' % template_object_name: page_obj.object_list,
             'paginator': paginator,
             'page_obj': page_obj,
@@ -92,14 +91,14 @@ def object_list(request, queryset, paginate_by=None, page=None,
             'pages': paginator.num_pages,
             'hits': paginator.count,
             'page_range': paginator.page_range,
-        }, context_processors)
+        }
     else:
-        c = RequestContext(request, {
+        c = {
             '%s_list' % template_object_name: queryset,
             'paginator': None,
             'page_obj': None,
             'is_paginated': False,
-        }, context_processors)
+        }
         if not allow_empty and len(queryset) == 0:
             raise Http404
 
@@ -113,7 +112,7 @@ def object_list(request, queryset, paginate_by=None, page=None,
         template_name = "%s/%s_list.html" % (model._meta.app_label,
                                              model._meta.object_name.lower())
     t = template_loader.get_template(template_name)
-    return HttpResponse(t.render(c), content_type=content_type)
+    return HttpResponse(t.render(c, request=request), content_type=content_type)
 
 
 def object_detail(request, queryset, object_id=None, slug=None,
@@ -152,15 +151,15 @@ def object_detail(request, queryset, object_id=None, slug=None,
         t = template_loader.select_template(template_name_list)
     else:
         t = template_loader.get_template(template_name)
-    c = RequestContext(request, {
+    c = {
         template_object_name: obj,
-    }, context_processors)
+    }
     for key, value in extra_context.items():
         if callable(value):
             c[key] = value()
         else:
             c[key] = value
-    response = HttpResponse(t.render(c), content_type=content_type)
+    response = HttpResponse(t.render(c, request=request), content_type=content_type)
     return response
 
 
