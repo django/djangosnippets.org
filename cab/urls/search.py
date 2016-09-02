c617083fa413a0d45ff26c96751210901dfad7cf
Tim Graham <timograham@gmail.com>
2016-09-02 13:44:44 -0400

    Remove string views in urlpatterns

--- cab/urls/search.py
+++ cab/urls/search.py
@@ -1,20 +1,15 @@
 from django.conf.urls import url
-from haystack.views import SearchView, search_view_factory
+from haystack.views import SearchView, basic_search, search_view_factory
 
 from ..forms import AdvancedSearchForm
+from ..views.snippets import autocomplete
 
 search_view = search_view_factory(view_class=SearchView,
                                   template='search/advanced_search.html',
                                   form_class=AdvancedSearchForm)
 
 urlpatterns = [
-    url(r'^$',
-        'haystack.views.basic_search',
-        name='cab_search'),
-    url(r'^autocomplete/$',
-        'cab.views.snippets.autocomplete',
-        name='snippet_autocomplete'),
-    url(r'^advanced/$',
-        search_view,
-        name='cab_search_advanced'),
+    url(r'^$', basic_search, name='cab_search'),
+    url(r'^autocomplete/$', autocomplete, name='snippet_autocomplete'),
+    url(r'^advanced/$', search_view, name='cab_search_advanced'),
 ]
