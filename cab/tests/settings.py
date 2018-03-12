2141a8fba661e155c5228317e48ce353566040aa
Tim Graham <timograham@gmail.com>
2018-03-12 11:24:48 -0400

    Remove usage of useless SessionAuthenticationMiddleware

--- cab/tests/settings.py
+++ cab/tests/settings.py
@@ -15,7 +15,6 @@ MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 )
 
 HAYSTACK_CONNECTIONS = {
