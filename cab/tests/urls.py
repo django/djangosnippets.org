53622298bbcbcff66ae55a15c89f8fb30c474ddc
Chris Wedgwood <wedgemail@gmail.com>
2020-08-02 10:57:32 +0100

    fix tests for postgres

--- cab/tests/urls.py
+++ cab/tests/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 
 urlpatterns = [
+    path('accounts/', include('allauth.urls')),
     path('bookmarks/', include('cab.urls.bookmarks')),
     path('comments/', include('django_comments.urls')),
     path('feeds/', include('cab.urls.feeds')),
     path('languages/', include('cab.urls.languages')),
     path('popular/', include('cab.urls.popular')),
     path('search/', include('cab.urls.search')),
+    path('searchfulltext/', include('cab.urls.searchfulltext')),
     path('snippets/', include('cab.urls.snippets')),
     path('tags/', include('cab.urls.tags')),
     path('users/', include('cab.urls.users')),
