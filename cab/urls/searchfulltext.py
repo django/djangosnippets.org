530a1bbc7c1c92db147995fc8b606e06a79d7039
Chris Wedgwood <wedgemail@gmail.com>
2020-08-02 11:16:44 +0100

    isort

--- cab/urls/searchfulltext.py
+++ cab/urls/searchfulltext.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from ..views.snippets import advanced_search_full_text, autocomplete_full_text, basic_search_full_text
+from ..views.snippets import (
+    advanced_search_full_text, autocomplete_full_text, basic_search_full_text,
+)
 
 urlpatterns = [
     path('', basic_search_full_text, name='cab_search_full_text'),
