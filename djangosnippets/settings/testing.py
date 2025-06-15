67bb05babe7e8b350de25480ce574436028ea09f
Chris Wedgwood <wedgemail@gmail.com>
2025-06-15 21:48:03 +0100

    Upgrade to Django==4.0.10

--- djangosnippets/settings/testing.py
+++ djangosnippets/settings/testing.py
@@ -9,6 +9,7 @@ MIDDLEWARE = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django_htmx.middleware.HtmxMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 )
 
 
@@ -24,7 +25,7 @@ INSTALLED_APPS = (
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
-    "allauth.socialaccount.providers.bitbucket",
+    "allauth.socialaccount.providers.bitbucket_oauth2",
     "allauth.socialaccount.providers.github",
     "allauth.socialaccount.providers.twitter",
     "base",
