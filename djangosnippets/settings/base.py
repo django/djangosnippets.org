51dc95efc9d075f0b668687b3047a5fb5461aa16
antoliny0919 <antoliny0919@gmail.com>
2025-06-27 10:58:40 +0900

    Add persistent connections and health checks using database config.

--- djangosnippets/settings/base.py
+++ djangosnippets/settings/base.py
@@ -178,7 +178,12 @@ MESSAGE_TAGS = {
 }
 
 
-DATABASES = {"default": dj_database_url.config(default="postgres:///djangosnippets")}
+DATABASES = {
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
